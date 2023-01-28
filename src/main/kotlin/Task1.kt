315491fa3ffba42c416f31077b532814ac275b0c
AlenaTyurina <ifus55@mail.ru>
2023-01-28 17:10:37 +0300

    almost done commit

--- src/main/kotlin/Task1.kt
+++ src/main/kotlin/Task1.kt
@@ -1,12 +1,11 @@
 package ru.netology
 
 data class Post(
-    val id: Int,
-    val authorId: Int,
-    val authorName: String,
-    val content: String,
-    val published: Long,
-    val likes: Int,
+    var id: Int = 0,
+    var authorId: Int = 213,
+    val authorName: String = "Steve",
+    val content: String = "lolololoo",
+    var published: Long = 2345678956,
 
     val canEdit: Boolean = true,
     val postponedId: Int = 0,
@@ -18,26 +17,48 @@ data class Post(
     val markedAsAds: Boolean = false,
     val isPinned: Int = 0,
     val friendsOnly: Int = 0,
+    val comments: Comments = Comments(),
+    val likes: Likes = Likes()
 )
 
+data class Comments(
+    val count: Int = 9,
+    val canPost: Boolean = true,
+    val groupsCanPost: Boolean = false
+)
+data class Likes(
+    val count: Int = 29,
+    val canPublish: Boolean = true,
+)
 object WallService {
 
     private var posts = emptyArray<Post>()
+    private var ident = 5
 
-    fun add(post: Post): Array<Post> {
-        posts += post
-        return posts
+    fun add(post: Post): Post {
+        posts += post.copy(id = ident)
+        ident += 1
+        return posts.last()
     }
 
+    fun update(post: Post): Boolean {
+        for ((index, postIn) in posts.withIndex()) {
+             if (post.id == postIn.id) {
+                 posts[index] = post.copy(authorId = postIn.authorId, published = postIn.published)
+                return true
+            }
+        }
+        return false
+    }
 }
 
 fun main() {
-    val postOne = Post(1, 1, "autohr", "content", likes = 0, published = 0)
-    val postTwo = Post(1, 1, "autohr", "content", likes = 0, published = 0)
-
-//    val liked = post.copy(likes = post.likes + 1)
-//    val (id, authorId, _, content) = post
-//    println("$id, $authorId, $content")
-
-
+    var wall = WallService
+    var postOne = Post(1)
+    var postTwo = Post(1)
+    var postOTree = Post(1)
+    var postOFour = Post(5, authorId = 8, content = "fooo", published = 3456789)
+    var postOFive = Post(6, authorId = 0, content = "pooo", published = 34567)
 }
+
+
